7a451d84db8f5835e507999b102fd02dce361b5b
Githu77 <githubenard16@gmail.com>
2024-07-11 15:58:09 +0300

    add files

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+Route module for API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -20,29 +21,37 @@ if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """ if not found
     """
     return jsonify({"error": "Not found"}), 404
-    
+
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized error
+    """
+    For unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden error
+def unauthorized(error) -> str:
+    """
+    if forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
-    
+
+
 @app.before_request
 def before_request():
     """
-    handler before_request
+    the handler before_request
     """
     authorized_list = ['/api/v1/status/',
                        '/api/v1/unauthorized/', '/api/v1/forbidden/',
@@ -57,6 +66,7 @@ def before_request():
         request.current_user = auth.current_user(request)
         if not auth.current_user(request):
             abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python3
+"""
+The auth class
+"""
+
 from tabnanny import check
 from flask import request
 from typing import TypeVar, List
+from os import getenv
+User = TypeVar('User')
+
 
 class Auth:
-    """ This is to manage API authentication
     """
+    for API authentication
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ returns False - path and excluded_paths
+        """
+        to return False, path and excluded_paths
         """
         check = path
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
@@ -16,14 +27,25 @@ class Auth:
         if check in excluded_paths or path in excluded_paths:
             return False
         return True
-        
+
     def authorization_header(self, request=None) -> str:
-        """ returns None - request and is flask request object
+        """
+        to return None - request
         """
         if request is None:
-            return none
+            return None
         return request.headers.get("Authorization")
-        
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ returns None - request and is flask request object
+
+    def current_user(self, request=None) -> User:
         """
+        returns None - request
+        """
+        return None
+
+    def session_cookie(self, request=None):
+        """
+        to return cookie from a request
+        """
+        if request:
+            session_name = getenv("SESSION_NAME")
+            return request.cookie.get(session_name, None)
